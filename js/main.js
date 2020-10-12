16deb43e50fe44ef9fea3049d4c5d72619d44b82
Mohd Saiful <mohd.saiful@live.com>
2020-10-12 22:42:28 +0800

    Added basic collision detection

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
+document.querySelector(".start_screen").style.display = "none";
+document.querySelector(".game").style.display = "block";
 
-// Game intro function
+let $problem = document.querySelector(".problem");
+let $player = document.querySelector(".player");
+let arr = [];
+arr.push($player);
+
+let $box = document.querySelector(".innerbox");
+$box.addEventListener("mousemove", trackPlayer);
+
 function playIntro() {
     document.querySelector(".start_screen").style.display = "block";
     document.querySelector(".game").style.display = "none"
     setInterval(function () { document.querySelector(".start_screen").style.display = "none" }, 3000);
     setInterval(function () { document.querySelector(".game").style.display = "block" }, 3000);
 }
-
-// Invoke intro
-// playIntro();
-
-document.querySelector(".start_screen").style.display = "none";
-document.querySelector(".game").style.display = "block";
-
-let $box = document.querySelector(".innerbox");
-$box.addEventListener("mousemove", trackPlayer);
-
-let $problem = document.querySelector(".problem");
 
 function trackPlayer(e) {
     let left = e.offsetX;
@@ -37,41 +35,7 @@ function makeNewPos() {
     return [randomHeight, randomWidth];
 }
 
-// function simpleMove() {
-//     let position = makeNewPos();
-
-//     setInterval(frame, 5);
-
-//     let speed = 0.8;
-
-//     function frame() {
-//         if (position[0] > $box.clientHeight || position[1] > $box.clientWidth) {
-//             speed = speed * -1;
-//             position[0] += speed;
-//             position[1] += speed;
-//             $problem.style.top = position[0] + "px";
-//             $problem.style.left = position[1] + "px";
-
-//         } else if (position[0] < $box.clientTop || position[1] < $box.clientLeft) {
-//             speed = Math.abs(speed);
-//             position[0] += speed;
-//             position[1] += speed;
-//             $problem.style.top = position[0] + "px";
-//             $problem.style.left = position[1] + "px";
-//         }
-//         else {
-//             position[0] += speed;
-//             position[1] += speed;
-//             $problem.style.top = position[0] + "px";
-//             $problem.style.left = position[1] + "px";
-//         }
-//         console.log($problem.style.left)
-//         console.log($problem.style.top)
-//     }
-// }
-
-
-function randomMove(problem) {
+function randomMove(virus) {
 
     let postion = makeNewPos();
     let y = postion[0] + "px";
@@ -87,34 +51,65 @@ function randomMove(problem) {
 
 
 
-    problem.animate([
-        // keyframes
+    virus.animate([
         { transform: `translate(${x}, ${y})` },
-        { transform: `translate(${x1}, ${y1})` },
+        { transform: `translate(${x1}, ${$box.clientHeight}px)` },
         { transform: `translate(${x2}, ${y2})` },
-        { transform: `translate(${x}, ${y})` }
-
+        { transform: `translate(${$box.clientWidth}px, ${y1})` },
+        { transform: `translate(${$box.clientLeft}px, ${y2})` },
+        { transform: `translate(${x1}, ${$box.clientTop}px)` },
+        { transform: `translate(${x}, ${y})` },
     ], {
-        // timing options
-        duration: 8000,
+        duration: 10000,
         iterations: Infinity,
-        // iterationComposite: "accumulate"
     });
+
+
 }
 
-function createProblems() {
-    let $createProblem = document.createElement("div");
-    $createProblem.className = "problem";
-    randomMove(document.querySelector(".box").appendChild($createProblem));
+function createVirus() {
+
+    let $createVirus = document.createElement("div");
+    $createVirus.className = "virus";
+    let uniqueID = Number($createVirus.id) + 1;
+    $createVirus.id = uniqueID;
+    randomMove(document.querySelector(".box").appendChild($createVirus));
+    arr.push($createVirus);
 }
 
-createProblems();
+function detectCollision(element) {
+
+    setInterval(ifCollide, 5)
+
+    function ifCollide() {
+
+        let x1 = element[0].getBoundingClientRect().x;
+        let y1 = element[0].getBoundingClientRect().y;
+
+        for (let i = 1; i < element.length; i++) {
+
+            let x2 = element[i].getBoundingClientRect().x;
+
+            let y2 = element[i].getBoundingClientRect().y;
+            let xDistance = x2 - x1;
+            let yDistance = y2 - y1;
+
+            d = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+
+            if (d < 50) {
+
+                console.log("You're hit")
+
+            }
+        }
+    };
+
+}
 
 
+setInterval(createVirus, 3000);
 
 
+console.log(arr)
 
-
-
-
-
+// detectCollision(arr);
